8d8811ad63f2a0d62ee96b07fd0b48277b261048
Muhammad Ahsin <ahsin.and@gmail.com>
2023-02-16 17:29:26 +0700

    route update

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { createQuiz, getQuizByUUID } from "../controllers/quiz.js";
+import {
+  createQuiz,
+  getQuizByUUID,
+  quizList,
+  updateQuiz,
+} from "../controllers/quiz.js";
+
+import { submitAnswer, getResultByUser } from "../controllers/answer.js";
 
 // Init express routers
 const router = express.Router();
 
 router.post("/api/quiz", createQuiz);
 router.get("/api/quiz/:uuid", getQuizByUUID);
+router.get("/api/quiz", quizList);
+router.put("/api/quiz/:uuid", updateQuiz);
+
+router.post("/api/answers", submitAnswer);
+router.get("/api/results/:userUUID/:quizUUID", getResultByUser);
 
 export default router;
